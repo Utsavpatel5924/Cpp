a67ef594c2f95a5e6e3b93c88852fe056da7bc39
Utsav12345678900000000000 <ustavpatel2004@gmail.com>
2023-08-05 17:07:57 +0530

    5-8-2023

---
+++ atm2.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+class ATM
+
+{
+
+protected:
+    int cb; // current balance
+    int a; // amount
+    int w; // withdraw
+    int d; // deposit
+
+public:
+    int current()
+    {
+
+        return cb = 10000;
+    }
+
+    int withdraw()
+    {
+        cout << "Enter amount to be withdrawn :" << endl;
+        cin >> w;
+
+        if (w > cb)
+        {
+            cout << "Insufficient balance " << endl;
+        }
+        else
+        {
+            cout << "Your remaining balance :" << cb - w << endl;
+
+            cb -= w;
+        }
+    }
+    int deposit()
+    {
+        cout << "Enter amout to Deposit :" << endl;
+        cin >> d;
+        cout << "Your remaining balance :" << cb + d << endl;
+        cb += d;
+    }
+
+    int balance()
+    {
+        cout << "Your balance is :" << cb << endl;
+    }
+};
+
+int main()
+{
+    ATM a;
+    a.current();
+    cout << "Your current balance is :" << a.current() << endl;
+    int ch;
+    for (int i = 1; i!=(ch==4); i++)
+    {
+        int ch;
+        cout << "1.withdraw cash " << endl;
+        cout << "2.Deposit cash " << endl;
+        cout << "3.Show balance " << endl;
+        cout << "4.Exit" << endl;
+
+        cout<<"Enter your choice :"<<endl;
+        cin >> ch;
+
+        switch (ch)
+        {
+        case 1:
+            a.withdraw();
+            break;
+
+        case 2:
+            a.deposit();
+            break;
+
+        case 3:
+            a.balance();
+            break;
+
+        case 4:
+              
+              break;
+        default:
+        cout<<"Wrong choice entered "<<endl;  
+            break;
+        }
+
+        if(ch==4){
+            break;
+        }
+    }
+}
